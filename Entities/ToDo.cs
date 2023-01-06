b5a2860ab790c03800f0634064509af4ac15d9c1
Samcolserra <samcolserra@gmail.com>
2023-01-06 15:51:05 +0200

    Update ToDo.cs
    Id renamed to toDoId, added userId as parameter to constructor.

--- Entities/ToDo.cs
+++ Entities/ToDo.cs
@@ -2,7 +2,7 @@ namespace ToDoApp.Entities
 {
     public class ToDo
     {
-        public Guid _id {get; private set;}
+        public Guid _toDoId {get; private set;}
         public string _name {get; private set;}
         public string? _description {get; private set;}
         public Guid _userId {get; private set;}
@@ -10,16 +10,16 @@ namespace ToDoApp.Entities
         public DateTimeOffset _updatedTimestamp {get; private set;}
         public Status _status {get; private set;}
 
-        public ToDo(string name, Status status, string? description = null)
+        public ToDo(Guid userId, string name, Status status, string? description = null)
         {
+            _userId = userId;
             _name = name;
             _status = status;
             _description = description;
 
-            _id = Guid.NewGuid();
-            _userId = Guid.NewGuid();
+            _toDoId = Guid.NewGuid();
             _createdTimestamp = DateTimeOffset.UtcNow;
-            _updatedTimestamp = DateTimeOffset.UtcNow;
+            _updatedTimestamp = _createdTimestamp;
         }
     }
 }
