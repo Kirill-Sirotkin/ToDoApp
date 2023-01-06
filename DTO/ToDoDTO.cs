ce8aca17fc1020939844da846977fd1d8e843d6a
Samcolserra <samcolserra@gmail.com>
2023-01-06 19:49:01 +0200

    Fixed ToDoDTO
    Fields can now be properly httpposted.

--- DTO/ToDoDTO.cs
+++ DTO/ToDoDTO.cs
@@ -6,8 +6,9 @@ namespace ToDoApp.DTOs
     public class ToDoDTO
     {
         [Required]
-        public string? _name {get; private set;}
-        public string? _description {get; private set;}
-        public Status _status {get; private set;}
+        public string? _name {get; set;}
+        public string? _description {get; set;}
+        [Required]
+        public Status _status {get; set;}
     }
 }
