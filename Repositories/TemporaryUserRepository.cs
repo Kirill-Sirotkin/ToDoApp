a9c4c4fda211cf51ec961b59391963e05b851c70
Samcolserra <samcolserra@gmail.com>
2023-01-07 15:25:03 +0200

    Update TemporaryUserRepository.cs
    Implemented repo interface into user repo.

--- Repositories/TemporaryUserRepository.cs
+++ Repositories/TemporaryUserRepository.cs
@@ -1,7 +1,41 @@
+using ToDoApp.Entities;
+
 namespace ToDoApp.Repositories
 {
-    public class TemporaryUserRepository
+    public class TemporaryUserRepository : IUserRepository
     {
-        
+        public List<User> _userList {get; private set;} = new()
+        {
+            new User("john@gmail", "password"),
+            new User("bob@gmail", "wordpass"),
+            new User("mary@gmail", "wordpass")
+        };
+
+        public void CreateUser(User user)
+        {
+            _userList.Add(user);
+        }
+
+        public void DeleteUser(Guid id)
+        {
+            int index = _userList.FindIndex(_user => _user._userId == id);
+            _userList.RemoveAt(index);
+        }
+
+        public User GetUser(Guid id)
+        {
+            return _userList.Where(user => user._userId == id).SingleOrDefault();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return _userList;
+        }
+
+        public void UpdateUserPassword(Guid id, string password)
+        {
+            int index = _userList.FindIndex(user => user._userId == id);
+            _userList[index].UpdatePassword(password);
+        }
     }
 }
