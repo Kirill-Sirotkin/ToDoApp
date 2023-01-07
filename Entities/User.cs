576208b68009fafa53cf973d49d5afe7a7ec9290
Samcolserra <samcolserra@gmail.com>
2023-01-07 15:09:11 +0200

    Update User.cs
    Added password hashing in User class constructor.

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
 namespace ToDoApp.Entities
 {
     public class User
@@ -5,17 +8,34 @@ namespace ToDoApp.Entities
         public Guid _userId {get; private set;}
         public string _email {get; private set;}
         public string _passwordHash {get; private set;}
+        public byte[] _salt {get; private set;}
         public DateTimeOffset _createdTimestamp {get; private set;}
         public DateTimeOffset _updatedTimestamp {get; private set;}
 
-        public User(string email, string passwordHash)
+        public User(string email, string password)
         {
             _email = email;
-            _passwordHash = passwordHash;
+            _salt = GetSalt();
+            _passwordHash = GetHashedPassword(password, _salt);
 
             _userId = Guid.NewGuid();
             _createdTimestamp = DateTimeOffset.UtcNow;
             _updatedTimestamp = _createdTimestamp;
+        }
+        
+        public static byte[] GetSalt()
+        {
+            return RandomNumberGenerator.GetBytes(128 / 8);
+        }
+        public static string GetHashedPassword(string password, byte[] salt)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password,
+                salt,
+                KeyDerivationPrf.HMACSHA256,
+                10000,
+                256 / 8
+            ));
         }
     }
 }
