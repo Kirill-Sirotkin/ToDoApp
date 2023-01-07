b27af46a68c9cd0a4f22283c3108f37012c50fae
Samcolserra <samcolserra@gmail.com>
2023-01-07 20:07:40 +0200

    Update AuthenticationController.cs
    Added signup and signin httpposts to authentication controller.

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -6,6 +6,15 @@ namespace ToDoApp.Controllers
     [Route("api/v1")]
     public class AuthenticationController : ControllerBase
     {
-
+        [HttpPost("signup")]
+        public IActionResult SignUp(string email, string password)
+        {
+            return Unauthorized();
+        }
+        [HttpPost("signin")]
+        public IActionResult SignIn(string email, string password)
+        {
+            return Unauthorized();
+        }
     }
 }
