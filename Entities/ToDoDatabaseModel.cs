ae181b2b76121342c991d7bdc37ebe5a22ce32a6
Samcolserra <samcolserra@gmail.com>
2023-01-09 19:38:11 +0200

    Update ToDoDatabaseModel.cs
    Changed properties of tododatabasemodel to be public.

--- Entities/ToDoDatabaseModel.cs
+++ Entities/ToDoDatabaseModel.cs
@@ -2,12 +2,12 @@ namespace ToDoApp.Entities
 {
     public class ToDoDatabaseModel
     {
-        public Guid Id {get; private set;}
-        public string _name {get; private set;}
-        public string? _description {get; private set;}
-        public DateTimeOffset _createdTimestamp {get; private set;}
-        public DateTimeOffset _updatedTimestamp {get; private set;}
-        public Status _status {get; private set;}
+        public Guid Id {get; set;}
+        public string _name {get; set;}
+        public string? _description {get; set;}
+        public DateTimeOffset _createdTimestamp {get; set;}
+        public DateTimeOffset _updatedTimestamp {get; set;}
+        public Status _status {get; set;}
 
         public Guid UserId {get; set;}
         public UserDatabaseModel User {get; set;}
