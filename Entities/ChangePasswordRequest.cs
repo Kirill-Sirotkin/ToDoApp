a1033ade47893588e71962a46161384aa2d2473e
Samcolserra <samcolserra@gmail.com>
2023-01-09 18:22:44 +0200

    Create ChangePasswordRequest.cs
    Created change password request entity for API calls.

---
+++ Entities/ChangePasswordRequest.cs
@@ -0,0 +1,8 @@
+namespace ToDoApp.Entities
+{
+    public class ChangePasswordRequest
+    {
+        public string _oldPassword {get; set;}
+        public string _newPassword {get; set;}
+    }
+}
